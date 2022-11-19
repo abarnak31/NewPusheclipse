4e10f94d7cbfc63b8feee6064eaa9521d0cb5da7
abarna31 <abarna.k2010@gmail.com>
2022-11-19 15:59:06 +0530

    hi

---
+++ Control/src/com/amazon/test/ForState.java
@@ -0,0 +1,20 @@
+package com.amazon.test;
+
+public class ForState {
+
+	public static void main(String[] args) {
+		System.out.println("1 to 5 order");
+		int no = 1;
+		while ( no <= 5 ) {
+			System.out.println(no);
+			no++;
+		}
+		System.out.println("1 to 5 in reverse order");
+		int n = 5;
+		while ( n >= 1 ) {
+			System.out.println(n);
+			n--;
+		}
+
+	}
+}
